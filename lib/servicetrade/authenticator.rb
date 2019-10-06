b7d1d2e1e63af84c9013c801165ef89a00bc571f
Nick Sandberg <nss118@gmail.com>
2019-10-05 20:29:32 -0700

    storing user on authenticator

--- lib/servicetrade/authenticator.rb
+++ lib/servicetrade/authenticator.rb
@@ -2,26 +2,29 @@
 module Servicetrade
 
   class Authenticator
-    attr_writer :user, :password
+    attr_writer :username, :password
+    attr_reader :user
 
 
-    def initialize(user, password)
-      @user = user
+    def initialize(username, password)
+      @username = username
       @password = password
       @auth_token = 'empty'
+      @user = nil
     end
 
     def get_auth_token
       begin
         response = RestClient.post 'https://api.servicetrade.com/api/auth', 
-                        { username: @user,
+                        { username: @username,
                           password: @password }
       rescue
         self.set_auth_token = "Error:Invalid/Missing Credentials"
         puts "Error:Invalid/Missing Credentials"
       end
       unless response.nil?
-        self.set_auth_token = JSON.parse(response.body)["data"]["authToken"]
+        @auth_token = JSON.parse(response.body)["data"]["authToken"]
+        @user = JSON.parse(response.body)["data"]["user"]
       end
     end
 
@@ -29,11 +32,6 @@ module Servicetrade
       @auth_token
     end
 
-    private
-
-      def set_auth_token=(token_string)
-        @auth_token = token_string
-      end
 
   end
 end
