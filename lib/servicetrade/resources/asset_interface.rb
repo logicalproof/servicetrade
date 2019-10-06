94c4d997ddcbb452cee7239c451a9d8e66626d20
Nick Sandberg <nss118@gmail.com>
2019-10-06 10:25:35 -0700

    added asset interface2

--- lib/servicetrade/resources/asset_interface.rb
+++ lib/servicetrade/resources/asset_interface.rb
@@ -19,7 +19,7 @@ module Servicetrade
                 status: { type: "string", optional: true, allowed_values: ['active', 'inactive'], message: "comma-separated list of statuses: 'active', 'inactive' (will default to 'active' if not supplied)" },
                 parentId: { type: "integer", optional: true, allowed_values: [], message: "returns all assets subassets of the given parent asset" },
                 name: { type: "string", optional: true, allowed_values: [], message: "returns all assets whose name includes this string (case insensitive)"},
-                search: { type: "string", optional: true, allowed_values: []. message: "Performs simple search across asset text fields" }
+                search: { type: "string", optional: true, allowed_values: [], message: "Performs simple search across asset text fields" }
               }
     end
 
