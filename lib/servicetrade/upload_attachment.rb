c2335199e1dca1043d08cbb38be5653fa89b0e02
Nick Sandberg <nss118@gmail.com>
2019-10-06 15:35:28 -0700

    upload attachment object2

--- lib/servicetrade/upload_attachment.rb
+++ lib/servicetrade/upload_attachment.rb
@@ -4,7 +4,7 @@ module ServiceTrade
     def self.upload_job_paperwork(job_id, file, authenticator)
       action = Servicetrade::Action::PrepareAttachment.new
       api_interface = Servicetrade::ApiInterface.new(action, authenticator)
-      api_interface.post(purposeId: 1, entityId: job_id, entityType: 3, myfile: file)
+      api_interface.post(purposeId: 1, entityId: job_id, entityType: 3, file: file)
     end
 
 
