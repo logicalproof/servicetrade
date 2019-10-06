4710e7a930ba887ecc3bcd6c1876c1b03098751e
Nick Sandberg <nss118@gmail.com>
2019-10-05 19:47:43 -0700

    adding clock out

---
+++ lib/servicetrade/actions/clock_out.rb
@@ -0,0 +1,35 @@
+module Servicetrade
+  module Action
+    class ClockOut
+      attr_reader :url
+
+      def initialize(job_id, event_id=nil)
+        @job_id = job_id
+        if event_id
+          @event_id = "/" + event_id 
+        else
+          @event_id = ""
+        end
+        @url = base_url
+      end
+
+      def allowed_verbs
+        return ["POST"]
+      end
+      
+      def base_url
+        return "/job/#{@job_id}/clockout#{@event_id}"
+      end
+
+      def name
+        return "events"
+      end
+
+      def post_params
+        return {  lat: {type: "float", optional: true, allowed_values: [], message: "GPS latitude"},
+                  lon: {type: "float", optional: true, allowed_values: [], message: "GPS longitude"}
+                }
+      end
+    end
+  end
+end
