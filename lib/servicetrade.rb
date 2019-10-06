acc8febfcdb6cbb4654a966930322db7abadae54
Nick Sandberg <nss118@gmail.com>
2019-10-05 19:48:41 -0700

    adding clock out2

--- lib/servicetrade.rb
+++ lib/servicetrade.rb
@@ -14,7 +14,11 @@ module Servicetrade
   require 'servicetrade/authenticator'
   require 'servicetrade/response_processor'
   require 'servicetrade/resource_interface'
-  require 'servicetrade/resources'
+  require 'servicetrade/resources/tag_interface'
+  require 'servicetrade/resources/job_interface'
+  require 'servicetrade/resources/deficiency_interface'
+  require 'servicetrade/resources/location_interface'
+  require 'servicetrade/resources/clock_event_interface'
 
   require 'servicetrade/actions/clock_in'
   require 'servicetrade/actions/clock_in'
