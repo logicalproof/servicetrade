18536e4ab1eca47f34d67e6f5075011330a3d46b
Nick Sandberg <nss118@gmail.com>
2020-01-24 21:18:41 -0700

    added post and puts to assets

--- lib/servicetrade/resources/asset_interface.rb
+++ lib/servicetrade/resources/asset_interface.rb
@@ -2,7 +2,7 @@ module Servicetrade
   class AssetInterface < Servicetrade::ResourceInterface
     
     def allowed_verbs
-      return ["GET"]
+      return ["GET", "POST", "PUT"]
     end
     
     def base_url
@@ -24,5 +24,21 @@ module Servicetrade
     end
 
   end
+
+
+  def post_params
+      post_parameters = self.put_params
+      post_parameters[:locationId] = {type:"integer", optional: false, allowed_values:[], message: "Location to which the asset belongs. Required for POST. Supported on POST only."}
+      post_parameters[:type] = {type:"string", optional: false, allowed_values:[], message: "Type of asset. Required for POST. Supported on POST only."}
+      return post_parameters
+    end
+
+    def put_params
+      return {properties: {type:"object", optional: false, allowed_values:[], message: "Set of key/value property pairs specific to this type of asset. Required for POST."},
+              status: {type:"string", optional: true, allowed_values:[], message: "Status of asset; one of: 'active' or 'inactive'. If not provided for POST, defaults to 'active'."},
+              orderIndex: {type:"integer", optional: true, allowed_values:[], message: "Items are listed from low to high value, null is the default value. Optional"},
+              parentId: {type:"integer", optional: true, allowed_values:[], message: "Sets the parent asset of this asset. Parent asset type must be allowable for this asset type."}
+              }
+    end
 
 end
