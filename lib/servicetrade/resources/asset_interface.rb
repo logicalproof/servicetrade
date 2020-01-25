0b0bfdb1196e16956775f5886b202489864be186
Nick Sandberg <nss118@gmail.com>
2020-01-24 21:34:57 -0700

    v

--- lib/servicetrade/resources/asset_interface.rb
+++ lib/servicetrade/resources/asset_interface.rb
@@ -14,17 +14,23 @@ module Servicetrade
     end
 
     def get_params
-      return {  locationId: { type: "integer", optional: true, allowed_values: [], message: "returns all assets at the given location" },
-                type: { type: "string", optional: true, allowed_values: [], message: "returns all assets of the given type" },
-                status: { type: "string", optional: true, allowed_values: ['active', 'inactive'], message: "comma-separated list of statuses: 'active', 'inactive' (will default to 'active' if not supplied)" },
-                parentId: { type: "integer", optional: true, allowed_values: [], message: "returns all assets subassets of the given parent asset" },
-                name: { type: "string", optional: true, allowed_values: [], message: "returns all assets whose name includes this string (case insensitive)"},
-                search: { type: "string", optional: true, allowed_values: [], message: "Performs simple search across asset text fields" }
-              }
+      return {
+        locationId: { 
+          type: "integer", optional: true, allowed_values: [], message: "returns all assets at the given location" },
+        type: { 
+          type: "string", optional: true, allowed_values: [], message: "returns all assets of the given type" },
+        status: { 
+          type: "string", optional: true, allowed_values: ['active', 'inactive'], message: "comma-separated list of statuses: 'active', 'inactive' (will default to 'active' if not supplied)" },
+        parentId: { 
+          type: "integer", optional: true, allowed_values: [], message: "returns all assets subassets of the given parent asset" },
+        name: { 
+          type: "string", optional: true, allowed_values: [], message: "returns all assets whose name includes this string (case insensitive)"},
+        search: { 
+          type: "string", optional: true, allowed_values: [], message: "Performs simple search across asset text fields" }
+        }
     end
 
-
-  def post_params
+    def post_params
       post_parameters = self.put_params
       post_parameters[:locationId] = {type:"integer", optional: false, allowed_values:[], message: "Location to which the asset belongs. Required for POST. Supported on POST only."}
       post_parameters[:type] = {type:"string", optional: false, allowed_values:[], message: "Type of asset. Required for POST. Supported on POST only."}
@@ -32,11 +38,16 @@ module Servicetrade
     end
 
     def put_params
-      return {properties: {type:"object", optional: false, allowed_values:[], message: "Set of key/value property pairs specific to this type of asset. Required for POST."},
-              status: {type:"string", optional: true, allowed_values:[], message: "Status of asset; one of: 'active' or 'inactive'. If not provided for POST, defaults to 'active'."},
-              orderIndex: {type:"integer", optional: true, allowed_values:[], message: "Items are listed from low to high value, null is the default value. Optional"},
-              parentId: {type:"integer", optional: true, allowed_values:[], message: "Sets the parent asset of this asset. Parent asset type must be allowable for this asset type."}
-              }
+      return {
+        properties: {
+          type:"object", optional: false, allowed_values:[], message: "Set of key/value property pairs specific to this type of asset. Required for POST."},
+        status: {
+          type:"string", optional: true, allowed_values:[], message: "Status of asset; one of: 'active' or 'inactive'. If not provided for POST, defaults to 'active'."},
+        orderIndex: {
+          type:"integer", optional: true, allowed_values:[], message: "Items are listed from low to high value, null is the default value. Optional"},
+        parentId: {
+          type:"integer", optional: true, allowed_values:[], message: "Sets the parent asset of this asset. Parent asset type must be allowable for this asset type."}
+        }
     end
-
+  end
 end
--- lib/servicetrade/version.rb
+++ lib/servicetrade/version.rb
@@ -1,3 +1,3 @@
 module Servicetrade
-  VERSION = "0.2.4.pre"
+  VERSION = "0.2.5.pre"
 end
